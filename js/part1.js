7c435c917f029802277496d41a390b679ad6abd3
Danimal-63 <daniel_guerrero_73@yahoo.com>
2020-01-20 19:05:09 -0800

    parts 1 and 2 completed of base conversion

--- js/part1.js
+++ js/part1.js
@@ -1,14 +1,72 @@
 function ConversionPart1() {
 
-    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
+    var UnsignedInt = (document.getElementById("1_UnsignedInt").value);
     var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
     var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
 
-
-
-  var outputValue = "11111111";
-
+var outputValue=convertFromBase10(convertToBase10(UnsignedInt,UnsignedIntBaseFrom),UnsignedIntBaseTo);
   // Show the output on the screen
   FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
 
+}
+function convertToBase10(UnsignedInt,UnsignedIntBaseFrom){
+var outputValue;
+var nextDig=0;
+UnsignedInt+="";
+  while (UnsignedInt.length!=0){
+    nextDig=UnsignedInt.substring(0,1);
+    if (nextDig=="A"){
+      nextDig=10;
+    }
+    if (nextDig=="B"){
+      nextDig=11;
+    }if (nextDig=="C"){
+      nextDig=12;
+    }if (nextDig=="D"){
+      nextDig=13;
+    }if (nextDig=="E"){
+      nextDig=14;
+    }if (nextDig=="F"){
+      nextDig=15;
+    }
+    if (outputValue==null){
+      outputValue=nextDig;
+    }else{
+    outputValue=parseInt(outputValue*UnsignedIntBaseFrom)+parseInt(nextDig);
+  }
+    UnsignedInt=UnsignedInt.substring(1,UnsignedInt.length);
+  }
+  return outputValue;
+}
+function convertFromBase10(base10, UnsignedIntBaseTo){
+  var val;
+  var nextDig;
+  while (base10>0){
+    nextDig=parseInt(base10 % UnsignedIntBaseTo);
+    base10=parseInt(base10 / UnsignedIntBaseTo);
+    if (nextDig==10){
+      nextDig="A";
+    }
+    if (nextDig==11){
+      nextDig="B";
+    }
+    if (nextDig==12){
+      nextDig="C";
+    }
+    if (nextDig==13){
+      nextDig="D";
+    }
+    if (nextDig==14){
+      nextDig="E";
+    }
+    if (nextDig==15){
+      nextDig="F";
+    }
+    if (val==null){
+      val=nextDig;
+    }else{
+    val=nextDig+""+val;
+  }
+}
+  return val;
 }
--- js/part2.js
+++ js/part2.js
@@ -3,10 +3,54 @@ function ConversionPart2() {
     var SignedDecimalInt = parseInt(document.getElementById("2_SignedInt").value);
 
 
-
-    var outputValue = "010110101000110110011101";
-    var outputValueTwosComplement = "101001010111001001100011";
+    if (SignedDecimalInt>0){
+      var outputValue=convertFromBase10(SignedDecimalInt,2)
+      while(outputValue.length<32){
+        outputValue=0+""+outputValue;
+      }
+      var outputValueTwosComplement=twosComp(outputValue)
+    }else{
+      SignedDecimalInt=SignedDecimalInt*-1;
+      var outputValueTwosComplement=convertFromBase10(SignedDecimalInt,2)
+      while(outputValueTwosComplement.length<32){
+        outputValueTwosComplement=0+""+outputValueTwosComplement;
+      }
+      var outputValue = twosComp(outputValueTwosComplement);
+      SignedDecimalInt=SignedDecimalInt*-1;
+    }
 
     // Show the output on the screen
     FormatAndShowOutput([outputValue, outputValueTwosComplement, SignedDecimalInt], 2);
+}
+function twosComp(base2){
+  while(base2.length<32){
+    base2=0+""+base2;
+  }
+  var output=""
+  var nextDig=""
+  while (base2.length>0){
+      nextDig=base2.substring(0,1);
+      if (nextDig==1){
+        nextDig=0
+      }else{
+        nextDig=1
+      }
+      if (output==null){
+        output=nextDig;
+      }else{
+        output=output+nextDig;
+      }
+    base2=base2.substring(1,base2.length);
+  }
+  var count=output.length;
+  do{
+    nextDig=output.substring(count-1,count)
+    if (nextDig==0){
+      output=output.substring(0,count-1)+""+1+""+output.substring(count,output.length);
+    }else{
+      output=output.substring(0,count-1)+""+0+""+output.substring(count,output.length);
+    }
+    count=count-1;
+  }while(nextDig!=0 && count>0)
+    return output;
 }
